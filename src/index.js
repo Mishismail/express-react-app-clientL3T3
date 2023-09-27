0a1a2853b29cbb434627d704b6e87c036cbbaf8b
Mishismail <mishkaismail@icloud.com>
2023-09-27 23:52:50 +0200

    "Update"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App'; // Import your root component (e.g., App.js)
+import App from './App'; 
 import './App.css';
 
 const rootElement = document.getElementById('root');
